2e9147ddcc63abedef926d0210e599965da18262
nannipamula <131344842+nannipamula@users.noreply.github.com>
2024-09-12 14:05:27 +0530

    Update FIleController.cs

--- Template/Controllers/FIleController.cs
+++ Template/Controllers/FIleController.cs
@@ -21,7 +21,7 @@ namespace Template.Controllers
             if (userName == hardcodedUserName && passWord == hardcodedPassword)
             {
                 // Redirect to Index if the login is successful
-                return RedirectToAction("Index", "Home");
+                return RedirectToAction("LetterGenerate", "Home");
             }
 
             // If login fails, set an error message and return to login view
